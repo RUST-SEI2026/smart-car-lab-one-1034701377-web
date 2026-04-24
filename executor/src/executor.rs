3fa10d9c283135b6d89b2edc0b348474619cb552
1034701377-web <1034701377@qq.com>
2026-04-25 01:26:37 +0800

    pass test case should_return_heading_s_given_command_is_l_and_facing_is_w

--- executor/src/executor.rs
+++ executor/src/executor.rs
@@ -43,6 +43,7 @@ impl Executor {
                 'L' => match self.pose.heading {
                     'E' => self.pose.heading = 'N',
                     'S' => self.pose.heading = 'E',
+                    'W' => self.pose.heading = 'S',
                     _ => (),
                 },
                 _ => (),
