fc0256219efce1b89106942db160ea2d94ed5000
1034701377-web <1034701377@qq.com>
2026-04-25 01:17:20 +0800

    add test case: should_return_heading_e_given_command_is_l_and_facing_is_s

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -53,4 +53,13 @@ mod left_turn_tests {
         let expected_pose = Pose::new(0, 0, 'N');
         assert_eq!(expected_pose, executor.query());
     }
+
+    #[test]
+    fn should_return_heading_e_given_command_is_l_and_facing_is_s() {
+        let original_pose = Pose::new(0, 0, 'S');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("L");
+        let expected_pose = Pose::new(0, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+    }
 }
