3d6fd417ce6dd51c777ec5f5e47365ac4016a4a0
1034701377-web <1034701377@qq.com>
2026-04-25 00:42:44 +0800

    add test case: should_return_y_minus_1_given_command_is_m_and_facing_is_s

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -13,4 +13,13 @@ mod move_tests {
         let expected_pose = Pose::new(1, 0, 'E');
         assert_eq!(expected_pose, executor.query());
     }
+
+    #[test]
+    fn should_return_y_minus_1_given_command_is_m_and_facing_is_s() {
+        let original_pose = Pose::new(0, 0, 'S');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("M");
+        let expected_pose = Pose::new(0, -1, 'S');
+        assert_eq!(expected_pose, executor.query());
+    }
 }
